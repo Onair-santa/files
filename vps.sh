3945236e4ccbfc8dc45fd4603750915ba1bb0799
Onair <42511409+Onair-santa@users.noreply.github.com>
2024-08-05 03:39:02 +0500

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -778,7 +778,7 @@ sleep 1
 sudo rm /root/.config/synth-shell/synth-shell-greeter.config.default
 sudo rm /root/.config/synth-shell/synth-shell-greeter.config
 wget https://github.com/Onair-santa/files/main/synth-shell-greeter.config -q -O /root/.config/synth-shell/synth-shell-greeter.config
-source .bashrc
+source /root/.bashrc
 sleep 1
 }
 
