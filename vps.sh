eb9b8bba22428c195d10f2ed60a78291832fe241
Onair-santa <42511409+Onair-santa@users.noreply.github.com>
2025-04-03 04:09:55 +0500

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -721,7 +721,17 @@ synth_shell() {
 
     #3X-UI
 xui() {
-    bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+    wget https://github.com/Onair-santa/3X-UI-Debian11/releases/download/2.4.1/x-ui-linux-amd64.tar.gz
+    cd /root/
+    rm -rf x-ui/ /usr/local/x-ui/ /usr/bin/x-ui
+    tar zxvf x-ui-linux-amd64.tar.gz
+    chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+    cp x-ui/x-ui.sh /usr/bin/x-ui
+    cp -f x-ui/x-ui.service /etc/systemd/system/
+    mv x-ui/ /usr/local/
+    systemctl daemon-reload
+    systemctl enable x-ui
+    systemctl restart x-ui
 }
 
     # AmneziaWG
